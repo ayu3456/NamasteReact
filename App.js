54a9f6c54b258c04e134285f62c0d7ac693c82da
ayu3456 <coaderayush1309@gmail.com>
2025-02-24 12:13:13 +0530

    episode 1

--- App.js
+++ App.js
@@ -2,9 +2,23 @@
 
 const h1 = React.createElement("h1",{},"This is h1");
 const h2 = React.createElement("h2",{},"This is h2");
+const h3 = React.createElement("h3",{},"This is h3");
 
-const child1 = React.createElement("div",{id:"child1"},[h1,h2]);
-const parent = React.createElement("div",{id:"parent"},[child1])
+const h4 = React.createElement("h4",{},"This is H4");
+
+const h5 = React.createElement("h5",{},"This is h5");
+
+const h6 = React.createElement("h6",{},"This is h6");
+
+
+
+
+
+
+const child1 = React.createElement("div",{id:"child1"},[h1,h2,h3]);
+const child2 = React.createElement("div",{id:"child2"},[h4,h5,h6])
+
+const parent = React.createElement("div",{id:"parent"},[child1,child2])
 
 const heading = React.createElement(
   "div",
