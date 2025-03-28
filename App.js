80c3064847c550b58380a8dd6aa401ecc2edf70d
ayu3456 <coaderayush1309@gmail.com>
2025-03-29 02:25:50 +0530

    till dynamic display

--- App.js
+++ App.js
@@ -42,41 +42,50 @@ const Header = () => {
   );
 };
 
-const styleCard = {
-  backgroundColor: "#f0f0f0"
-}
+// const styleCard = {
+//   backgroundColor: "#f0f0f0",
+// };
 
-// InLine Style are given in the form of JS objects.
+// In-Line Style are given in the form of JS objects.
 
-const ResturantCard = () => {
+// you can do destructuring it 
+const ResturantCard = ({resName,cuisine}) => {
+  //console.log(resName,cuisine)
   return (
-    <div className="res-card" style={styleCard}>
-      <h3> Meghna Foods </h3>
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://static.wixstatic.com/media/91e241_475faa4fa56341f3878699dde5ab4904~mv2.jpg/v1/fill/w_666,h_444,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/91e241_475faa4fa56341f3878699dde5ab4904~mv2.jpg"
+      />
+      <h3>{resName}</h3>
+      <h3>{cuisine}</h3>
+      <h3>4.3 stars</h3>
+      <h3>38 minutes</h3>
     </div>
-  )
-
-}
+  );
+};
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div> 
-      <div className="res-container"></div>
-        <ResturantCard/>
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturantCard resName="Meghna Foods" cuisine="Biryani,North-Indian" />
+        <ResturantCard resName="KFC" cuisine="Burger,Fast-Food" />
+      </div>
     </div>
-  )
-
-}
-
-
-
-const Applayout = () => {
-  return <div className="app">
-    <Header/>
-    <Body/>
-
-  </div>;
+  );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Applayout/>);
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout />);
